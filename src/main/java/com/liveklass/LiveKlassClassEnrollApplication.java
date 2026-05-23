0909f17a3cdfe2119697e4f1e04e8b0ba23bb4dc
penrose0214 <penrose0214@gmail.com>
2026-05-23 19:44:50 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/com/liveklass/LiveKlassClassEnrollApplication.java
@@ -0,0 +1,12 @@
+package com.liveklass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveKlassClassEnrollApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveKlassClassEnrollApplication.class, args);
+    }
+}
